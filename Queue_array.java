bfe9130da2f57ca8f1eccb7ba9f495b5301b6f6e
SOUMITA MAITY <soumitamaity996@gmail.com>
2024-02-07 21:30:07 +0530

    first commit

---
+++ Queue_array.java
@@ -0,0 +1,58 @@
+public class Queue_array {
+    static class Queue{
+        static int arr[];
+        static int size;
+        static int rear=-1;
+        Queue(int n){
+            arr = new int[n];
+            this.size = n;
+        }
+        public static boolean isEmpty(){
+            return rear == -1;
+        }
+        //Enque
+        public static void add(int data){
+           if(rear == size-1){
+             System.out.println("queue is Full");
+             return;
+           }
+           rear++;
+           arr[rear]=data;
+        }
+
+        //Deque
+        public static int remove(){
+            if(isEmpty()){
+                System.out.println("Array is Empty");
+                return -1;
+            }
+            int front = arr[0];
+            for(int i=0;i<rear;i++){
+                arr[i]=arr[i+1];
+            }
+            rear--;
+            return front;
+        }
+
+        //Peek
+        public static int peek(){
+            if(isEmpty()){
+                System.out.println("queue is Empty");
+                return -1;
+            }
+            return arr[0];
+        }
+    }
+    public static void main(String[] args) {
+        Queue q = new Queue(5);
+        q.add(1);
+        q.add(2);
+        q.add(3);
+
+        while(!q.isEmpty()){
+            System.out.println(q.peek());
+            q.remove();
+        }
+        
+    }
+}
